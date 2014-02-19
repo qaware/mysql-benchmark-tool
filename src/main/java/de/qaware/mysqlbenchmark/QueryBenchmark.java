f173700c321c0f7491ac97741da7b1b4042ac2e0
FelixKelm <felix.kelm@qaware.de>
2014-02-19 10:22:11 +0100

    modified license file and licence headers added comments

--- src/main/java/de/qaware/mysqlbenchmark/QueryBenchmark.java
+++ src/main/java/de/qaware/mysqlbenchmark/QueryBenchmark.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2014 QAware GmbH (http://www.qaware.de/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package de.qaware.mysqlbenchmark;
 
 import de.qaware.mysqlbenchmark.jetm.CsvRenderer;
@@ -14,6 +30,8 @@ import java.sql.SQLException;
 import java.util.List;
 
 /**
+ * Executes a list of SQL statements. Results can be retrieved in the formats: JETM-formatting and CSV.
+ *
  * @author Felix Kelm felix.kelm@qaware.de
  */
 public class QueryBenchmark {
--- src/main/java/de/qaware/mysqlbenchmark/jetm/CsvRenderer.java
+++ src/main/java/de/qaware/mysqlbenchmark/jetm/CsvRenderer.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (C) 2014 QAware GmbH (http://www.qaware.de/)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package de.qaware.mysqlbenchmark.jetm;
 
 import etm.core.aggregation.Aggregate;
@@ -10,8 +26,10 @@ import java.text.NumberFormat;
 import java.util.*;
 
 /**
+ * Extending the JETM MeasurementRenderer for enabling CSV export of JETM measurements.
+ *
  * @author felix.kelm (felix.kelm@qaware.de)
- *         Date: 03.12.13
+ *
  */
 public class CsvRenderer implements etm.core.renderer.MeasurementRenderer {
 
